e7a80dab346afe753f71145578d7e22b1a441f68
DELL@Harshit <h4agarwahll@gmail.com>
2023-12-20 21:38:40 +0530

    -initial

--- SOLID_Design_Patterns/src/main/java/com/harshit1108/Single_Responsibility_Principle/AccountOperation.java
+++ SOLID_Design_Patterns/src/main/java/com/harshit1108/Single_Responsibility_Principle/AccountOperation.java
@@ -1,11 +1,9 @@
 package com.harshit1108.Single_Responsibility_Principle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AccountOperation{
 
     private static Map<Long,Account> accountlist = new HashMap<>();
-
-    private list
-
-
-
 }
