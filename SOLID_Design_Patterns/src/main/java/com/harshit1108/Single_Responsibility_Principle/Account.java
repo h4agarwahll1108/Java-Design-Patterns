4832ee3d53f3fcdd9219b0aec0559656b42e1b7b
DELL@Harshit <h4agarwahll@gmail.com>
2023-12-20 19:20:08 +0530

    Initial

---
+++ SOLID_Design_Patterns/src/main/java/com/harshit1108/Single_Responsibility_Principle/Account.java
@@ -0,0 +1,32 @@
+package com.harshit1108.Single_Responsibility_Principle;
+
+public class Account {
+
+    private Long accountNumber;
+    private String name;
+    private double amount;
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
---
+++ SOLID_Design_Patterns/src/main/java/com/harshit1108/Single_Responsibility_Principle/AccountOperation.java
@@ -0,0 +1,11 @@
+package com.harshit1108.Single_Responsibility_Principle;
+
+public class AccountOperation{
+
+    private static Map<Long,Account> accountlist = new HashMap<>();
+
+    private list
+
+
+
+}
